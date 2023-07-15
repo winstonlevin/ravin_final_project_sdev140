505eb3f306c085e0bdb07839847652c65958aa1a
winstonlevin <wlevin@purdue.edu>
2023-07-15 19:40:07 -0400

    Initial commit

---
+++ cd_calculator.py
@@ -0,0 +1,26 @@
+DAYS_PER_YEAR = 365.
+MONTHS_PER_YEAR = 12
+
+
+def calculate_cd(principle: float, rate: float, term: float) -> (float, float):
+    '''
+    This function returns average monthly dividend and total amount at end of term.
+
+    :param principle: float, the starting capital, dollars
+    :param rate: float, the annual interest rate, fraction
+    :param term: float, the term, months
+    :return: (float, float), a Tuple of the mean monthly return and total at end of term
+    '''
+
+    total = principle * (1 + rate / DAYS_PER_YEAR) ** (term * DAYS_PER_YEAR / MONTHS_PER_YEAR)
+    mean_return = (total - principle) / term
+
+    return mean_return, total
+
+
+cd_options = {
+    '8-Month': (0.0512, 8),
+    '15-Month': (0.0512, 15),
+    '29-Month': (0.0405, 29)
+}
+ZZ
