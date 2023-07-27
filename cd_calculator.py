0b54032b4b60174ba4e2d8f2281daa67c5d61b95
winstonlevin <wlevin@purdue.edu>
2023-07-26 22:07:05 -0400

    Added comments and test cases.

--- cd_calculator.py
+++ cd_calculator.py
@@ -1,3 +1,10 @@
+"""
+This program runs a certificate of deposit (CD) calculator. The user can select a term of deposit (take from Purdue
+Federal Credit Union in July 2023:
+https://www.purduefed.com/Resources/Information/Rates/Deposit-Rates
+The user selects the desired term and the principal they want to invest. Multiple terms and values can be calculated
+prior to exiting the program.
+"""
 from typing import Optional, Union, Callable
 import tkinter as tk
 
@@ -6,11 +13,19 @@ DAYS_PER_YEAR = 365.
 MONTHS_PER_YEAR = 12
 TERM_DEFAULT = '[Select a Term]'
 TKINTER_WINDOW = Union[tk.Tk, tk.Toplevel]
-EMPTY_DISPLAY = '$        '
+EMPTY_DISPLAY = '$            '
 
 
 # DEFINE FUNCTIONS -----------------------------------------------------------------------------------------------------
 def unpack_images(image_strs: list[str], image_zoom: int = 1, image_shrink: int = 1):
+    """
+    Convert list of strings with image locations to tkinter PhotoImage objects.
+
+    :param image_strs: list[str], list of file locations for images
+    :param image_zoom: int, level of zoom (enlarge size of image, 1=original size)
+    :param image_shrink: int, level of shink (shrink size of image, 1=original size)
+    :return: list[tk.PhotoImage], list of tkinter PhotoImage objects
+    """
     images = []
     for image_str in image_strs:
         image = tk.PhotoImage(file=image_str).zoom(image_zoom).subsample(image_shrink)
@@ -19,18 +34,41 @@ def unpack_images(image_strs: list[str], image_zoom: int = 1, image_shrink: int 
 
 
 def generate_swapper(open_window: TKINTER_WINDOW, closed_window: TKINTER_WINDOW):
+    """
+    Generate callback to close "open_window" and open "closed_winder"
+
+    :param open_window: Open window to be withdrawn (not destoryed)
+    :param closed_window: Closed window to be open (assumes it is previously withdrawn)
+    :return: callable, swapper callback
+    """
     def swap_windows():
         open_window.withdraw()
         closed_window.deiconify()
     return swap_windows
 
 
-def validate_term(selection) -> bool:
+def validate_term(selection: tk.Variable) -> bool:
+    """
+    Ensure the "selection" variable has a valid entry.
+
+    :param selection: tk.Variable, CD term selection variable.
+    :return: bool, valid flag
+    """
     return selection.get() != TERM_DEFAULT
 
 
-def generate_validated_swapper(open_window: TKINTER_WINDOW, closed_window: TKINTER_WINDOW,
-                          validation_fun: Callable, validation_var: tk.Variable):
+def generate_validated_swapper(
+        open_window: TKINTER_WINDOW, closed_window: TKINTER_WINDOW,
+        validation_fun: Callable, validation_var: tk.Variable
+):
+    """
+    Generate a callback to swap windows only after validating. If validation fails, do nothing.
+    :param open_window: Open window to be withdrawn (not destoryed)
+    :param closed_window: Closed window to be open (assumes it is previously withdrawn)
+    :param validation_fun: Callable, function to validate "validation_var."
+    :param validation_var: tk.Variable, variable that must be validated
+    :return: callable, validated swapper callback
+    """
     swap_windows = generate_swapper(open_window, closed_window)
 
     def swap_after_validation():
@@ -40,6 +78,12 @@ def generate_validated_swapper(open_window: TKINTER_WINDOW, closed_window: TKINT
 
 
 def generate_exit_program(root):
+    """
+    Generate a callback to exit the program by destroying the root window.
+
+    :param root: tk.TK, root window
+    :return: callable, exit program callback
+    """
     def exit_program():
         root.destroy()
     return exit_program
@@ -61,7 +105,20 @@ def calculate_cd(principal: float, rate: float, term: float) -> (float, float):
     return mean_return, total
 
 
-def generate_entry_calculator(cd_options, cd_selection, principal_entry, dividends_display, total_display):
+def generate_entry_calculator(
+        cd_options: dict, cd_selection: tk.Variable, principal_entry: tk.Variable,
+        dividends_display: tk.Label, total_display: tk.Label
+):
+    """
+    Generate callback to calculate CD from data entered into GUI.
+
+    :param cd_options: dict, CD options dictionary
+    :param cd_selection: tk.Variable, key for CD options dictionary
+    :param principal_entry: tk.Variable, principal for CD
+    :param dividends_display: tk.Label, display for CD average monthly dividends
+    :param total_display: tk.Label, display for end-of-term capital
+    :return: callable, CD calculation callback
+    """
     def calculate_cd_from_entry():
         selection = cd_selection.get()
 
@@ -72,7 +129,8 @@ def generate_entry_calculator(cd_options, cd_selection, principal_entry, dividen
 
             dividends_display['text'] = f'${mean_return:.2f}'
             total_display['text'] = f'${total:.2f}'
-        except RuntimeError:
+
+        except ValueError:
             dividends_display['text'] = EMPTY_DISPLAY
             total_display['text'] = EMPTY_DISPLAY
 
@@ -125,7 +183,7 @@ window_selection = RootWindow(
 )
 
 window_calculation = Window(window_selection, title='Calculate CD Gains', images=['cd_image.png'], image_shrink=2)
-# window_calculation.withdraw()  # Not open by default
+window_calculation.withdraw()  # Not open by default
 
 # BUILD FIRST WINDOW (TERM SELECTION) ----------------------------------------------------------------------------------
 
@@ -145,7 +203,8 @@ term_menu = tk.OptionMenu(frame_options, term_selection, *term_options)
 term_menu.grid(row=1, column=1, columnspan=1, sticky='nw')
 
 # Display Image
-label_bank_graphic = tk.Label(master=frame_options, image=window_selection.images[0])
+label_bank_graphic = tk.Label(
+    master=frame_options, image=window_selection.images[0], text='Bank', compound='bottom')
 label_bank_graphic.grid(row=1, column=2, columnspan=1, sticky='e')
 
 # Button to Navigate to Next Window
@@ -170,27 +229,19 @@ entry_principal.grid(row=10, column=0, columnspan=10, sticky='w')
 
 # Right-most frame: Display for CD calculations
 frame_display = tk.Frame(window_calculation)
-frame_display.grid(row=0, column=50, rowspan=50, columnspan=1, sticky='e', padx=15, pady=5)
+frame_display.grid(row=0, column=50, columnspan=1, sticky='e', padx=15, pady=5)
 
-# Sub-frame for total
-frame_total = tk.Frame(frame_display)
-frame_display.grid(row=0, padx=5, pady=5, sticky='n')
+label_total = tk.Label(frame_display, text='Total Capital at End of Term:\n')
+label_total.grid(row=0, column=50, columnspan=1, sticky='ne')
 
-label_total = tk.Label(frame_total, text='Total Capital at End of Term:\n')
-label_total.grid(row=0, column=0, padx=5, pady=5)
+display_total = tk.Label(frame_display, text=EMPTY_DISPLAY)
+display_total.grid(row=4, column=50, sticky='ne')
 
-display_total = tk.Label(frame_total, text=EMPTY_DISPLAY)
-display_total.grid(row=5, column=0)
+label_dividends = tk.Label(frame_display, text='Average dividends per month:\n')
+label_dividends.grid(row=24, column=50, sticky='e')
 
-# Sub-frame for average dividends
-frame_dividends = tk.Frame(frame_display)
-frame_dividends.grid(row=50, padx=5, pady=5, sticky='s')
-
-label_dividends = tk.Label(frame_dividends, text='Average dividends per month:\n')
-label_dividends.grid(row=50, column=0, padx=5, pady=5)
-
-display_dividends = tk.Label(frame_dividends, text=EMPTY_DISPLAY)
-display_dividends.grid(row=55, column=0)
+display_dividends = tk.Label(frame_display, text=EMPTY_DISPLAY)
+display_dividends.grid(row=26, column=50, sticky='e')
 
 # Middle frame: Calculation button
 frame_calculate = tk.Frame(window_calculation)
@@ -203,7 +254,9 @@ button_to_calculate = tk.Button(
 )
 button_to_calculate.grid(row=0, column=0, sticky='n')
 
-label_cd_graphic = tk.Label(master=frame_calculate, image=window_calculation.images[0])
+label_cd_graphic = tk.Label(
+    master=frame_calculate, image=window_calculation.images[0], text='Padlock in front of money.', compound='bottom'
+)
 label_cd_graphic.grid(row=40, column=0, sticky='s')
 
 # Button to go back to selection window and choose a different term
@@ -217,10 +270,6 @@ button_to_exit = tk.Button(
     window_calculation, text='Exit', command=generate_exit_program(window_selection)
 )
 button_to_exit.grid(row=50, column=50, sticky='e')
-
-
-# Display Image
-# lock_graphic = tk.PhotoImage('cd_image.png')
 
 # RUN WINDOWS ----------------------------------------------------------------------------------------------------------
 window_selection.mainloop()
